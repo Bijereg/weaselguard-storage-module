e246760ab86b368a646ffeac7f0db76a45e2a5de
Sergey Belousov <bijereg@gmail.com>
2021-03-01 01:25:53 +1000

    Added limit check in getLastEvents

--- src/main/java/com/weaselguard/weaselguardstoragemodule/services/EventServiceImpl.java
+++ src/main/java/com/weaselguard/weaselguardstoragemodule/services/EventServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -42,6 +43,9 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Iterable<Event> getLastEvents(int limit) {
+        if (limit < 1) {
+            return new ArrayList<>();
+        }
         return this.eventRepository.findAllByOrderByRegistrationDatetimeDescIdDesc(PageRequest.of(0, limit));
     }
 
